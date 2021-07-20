c178520e6eaf32caf0b83568dd216fefc513c96f
Anthony Milan <anthonybmilan@gmail.com>
2021-07-21 00:14:19 +0200

    added font

---
+++ src/homePage/homePage.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react';
+import styles from './homePage.css';
+class HomePage extends Component{
+    render(){
+        return(
+            <div className="Home">
+            <h1>Your Audio Library!</h1>
+            </div>
+        )
+    }
+}
+export default HomePage;
